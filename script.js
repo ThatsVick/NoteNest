832772eb63f4ebf514a42eb194afd182a1c859c3
ThatsVick <viktor.zimmermann@live.de>
2026-09-21 14:48:17 +0200

    termin update

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ async function renderStartDashboard() {
     ];
 
     if (cardTitle) {
-        cardTitle.innerText = `📅 Anstehende Aufgaben / Termine (${monthNames[currentMonth]} ${currentYear})`;
+        cardTitle.innerText = `📅 Anstehende Aufgaben / Ereignise (${monthNames[currentMonth]} ${currentYear})`;
     }
 
     const events = await getEventsData();
